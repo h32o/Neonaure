88e3a7149a2c4db44522c0ea9c70618e29556c32
h32o <DelplaceHugo@proton.me>
2026-06-09 22:40:16 +0200

    W.I.P. JSON Loading, Skeletic Controller + W.I.P. gitignore
    The work in progress json loader should at the end handles different file path, ones with just the names in it, one if it absolute or not etc. The ignore is pretty barebones and will worked upon later. Everything need to be checked by my collegues. Abstract Controller

---
+++ controller/controller.py
@@ -0,0 +1,8 @@
+
+#! temp
+#*  import the view (when implemented)
+#*  import the model
+
+class Controller() :
+    def __init__(self) : 
+        pass
---
+++ controller/functools/json_handler.py
@@ -0,0 +1,54 @@
+import json
+import os
+import logging
+from typing import Union, Dict
+
+# Subject to change
+DEFAULT_DIR : str = os.path.join("..","..","..","examples")  #! Example folder
+
+class JSONLoader:
+    
+    @staticmethod
+    def _resolve_path(input_path: str) -> str:
+        """
+        Private helper
+        Resolves absolute path / relative path vs just a file name (uses DEFAULT_DIR)
+        """
+
+        if os.path.isabs(input_path):
+            return input_path
+        
+        #? os.sep ? See : https://docs.python.org/3/library/os.html#os.sep ('/' or '\\')
+        if os.sep in input_path or '/' in input_path:
+            return os.path.join(os.getcwd(), input_path) #? os.getcwd() Return a string representing the current working directory.
+
+        else : 
+            return os.path.join(DEFAULT_DIR, input_path)
+    
+    @staticmethod
+    def load_json(file_path: str) -> Union[Dict, list] or None:
+        """
+        Loads data from a JSON file. 
+        """
+
+        resolved_file_path = JSONLoader._resolve_path(file_path)
+
+        try: 
+            with open(resolved_file_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+            return data
+        except FileNotFoundError :
+            logging.error("The file doen't exist!")
+            return None
+
+    @staticmethod
+    def save_json() -> bool:
+        """
+        Well, it saves a JSON file into a specified path
+
+        Returns : 
+        True (Save sucessful)
+        False (Failed)
+        """
+        pass
+
