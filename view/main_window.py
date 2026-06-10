d4b0c2254f5bdf46c7965e9556c35250b476f2ae
vxrli <dufour.arthur2910@gmail.com>
2026-06-10 20:50:26 +0200

    refactor: extract cell_widget and grid_widget from main_window
    Addition of patterns function and cell color

--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QLineEdit, QMessageBox, QLabel, QPushButton, QHBoxLayout
-from PyQt6.QtCore import Qt, QRegularExpression, QTimer, pyqtSignal
-from PyQt6.QtGui import QAction, QRegularExpressionValidator 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QMessageBox, QLabel, QPushButton, QHBoxLayout
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal
+from PyQt6.QtGui import QAction, QKeySequence
+from components.grid_widget import GridWidget
 
 class MainWindow(QMainWindow):
     signal_load_grid = pyqtSignal()
@@ -9,7 +10,6 @@ class MainWindow(QMainWindow):
     signal_reset_grid = pyqtSignal()
     signal_solve_grid = pyqtSignal()
     signal_undo = pyqtSignal()
-    signal_cell_changed = pyqtSignal(int, int, str)
 
     def __init__(self):
         super().__init__()
@@ -25,16 +25,14 @@ class MainWindow(QMainWindow):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_timer_display)
         
-        self.grid_layout = QGridLayout()
-        self.grid_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.grid_layout.setSpacing(0)
-        self.grid_layout.setContentsMargins(0, 0, 0, 0)
-        
-        self.main_layout.addLayout(self.grid_layout)
+        self.grid_widget = GridWidget()
+        self.main_layout.addWidget(self.grid_widget)
+        self.grid_widget.create_grid()
         
         self.bottom_layout = QHBoxLayout()
         
         self.undo_button = QPushButton("↶")
+        self.undo_button.setShortcut(QKeySequence("Ctrl+Z"))
         self.undo_button.setFixedSize(50, 50)
         self.undo_button.setStyleSheet("""
                 background-color: grey; 
@@ -57,9 +55,6 @@ class MainWindow(QMainWindow):
         self.bottom_layout.addWidget(self.solve_button)
         
         self.main_layout.addLayout(self.bottom_layout)
-        
-        self.cells = {}   
-        self.create_grid()
     
     def init_menu(self):
         menu_bar = self.menuBar() 
@@ -79,7 +74,7 @@ class MainWindow(QMainWindow):
         self._create_action(settings_menu, "&Game Rules", "Ctrl+H", self.show_rules)
 
         self.timer_label = QLabel("0s  ", self) 
-        self.timer_label.setStyleSheet("font-size: 13px; font-weight: bold; color: #ffffff;")
+        self.timer_label.setStyleSheet("font-size: 13px; font-weight: bold; color: #ffffff; margin-right: 70px;")
         self.timer_label.hide()  
         
         menu_bar.setCornerWidget(self.timer_label, Qt.Corner.TopRightCorner)
@@ -91,43 +86,15 @@ class MainWindow(QMainWindow):
         menu.addAction(action)
         return action 
 
-    def create_grid(self):
-        regex = QRegularExpression("^[1-5]$")
-        validator = QRegularExpressionValidator(regex, self)
-        
-        for line in range(8):
-            for column in range(8):
-                cell = QLineEdit()
-                cell.setValidator(validator)
-                cell.setMaxLength(1)
-                cell.setAlignment(Qt.AlignmentFlag.AlignCenter)
-                cell.setFixedSize(50, 50)   
-                cell.setStyleSheet("""
-                    background-color: white;
-                    border: 0.5px solid black;
-                    color: black;
-                    font-weight : bold;                  
-                """)
-                cell.row = line
-                cell.column = column
-                cell.textChanged.connect(self.on_cell_changed)
-                self.grid_layout.addWidget(cell, line, column)
-                self.cells[(line, column)] = cell
-
-    def on_cell_changed(self, text):
-        cell = self.sender()
-        if cell:
-            self.signal_cell_changed.emit(cell.row, cell.column, text)
-
     def show_rules(self):
         rules_box = QMessageBox(self)
-        rules_box.setWindowTitle("Règles du Néonaure")   
+        rules_box.setWindowTitle("Rules of the Néonaure")   
         rules_box.setText(
-            "Bienvenue dans le Néonaure !\n"
-            "Voici les règles pour résoudre la grille :\n\n"
-            "• un chiffre par case\n"
-            "• un chiffre doit être entouré de chiffres différents (y compris en diagonale)\n"
-            "• un motif de N cases (repéré en traits gras) doit comporter tous les chiffres de 1 à N\n"
+            "Welcome to the Néonaure!\n"
+            "Here are the rules for solving the grid:\n\n"
+            "• one number per cell\n"
+            "• a number must be surrounded by different numbers (including diagonally)\n"
+            "• a pattern of N cells (marked with bold lines) must contain all numbers from 1 to N\n"
         )
         rules_box.exec()
     
@@ -167,29 +134,8 @@ class MainWindow(QMainWindow):
             self.timer.start(1000)        
         else:
             self.timer_label.hide()       
-            self.timer.stop()         
+            self.timer.stop()
 
-    def set_index_cell(self, line, column, value):
-        cell = self.cells[(line, column)]
-        cell.setText(str(value))
-        cell.setReadOnly(True)
-        cell.setStyleSheet("""
-        background-color: #E0E0E0;
-        border: 0.5px solid black;
-        color: #333333;
-        font-weight: bold;
-         """)    
-    
-    def change_color_cell_error(self, line, column, is_error):
-        cell = self.cells[(line, column)]
-        if cell.isReadOnly():
-            return 
-        if is_error:
-            cell.setStyleSheet("background-color: #FFCCCC;")
-        else:
-            cell.setStyleSheet("background-color: white;")
-
-   
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
